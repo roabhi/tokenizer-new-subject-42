57ccc2664029917aeb84393f085b8c8719a207ee
roabhi <teskostudio@gmail.com>
2025-01-04 21:34:34 +0100

    Added minimal front-end integration. WIP for checking each project under each ring

---
+++ src/lib/constants/ringProjects.ts
@@ -0,0 +1,69 @@
+export const ringProjects = [
+  {
+    ringId: 0,
+    projects: [{ id: 1, name: 'libft', validated: false }],
+  },
+  {
+    ringId: 1,
+    projects: [
+      { id: 2, name: 'born_to_be_root', validated: false },
+      { id: 3, name: 'get_next_line', validated: false },
+      { id: 4, name: 'print_f', validated: false },
+    ],
+  },
+  {
+    ringId: 2,
+    projects: [
+      { id: 5, name: 'EXAM RANK 02', validated: false },
+      { id: 6, name: 'push_swap', validated: false },
+      { id: 7, name: 'minitalk', validated: false },
+      { id: 8, name: 'pipex', validated: false },
+      { id: 9, name: 'so_long', validated: false },
+      { id: 10, name: 'FdF', validated: false },
+      { id: 11, name: 'fract-ol', validated: false },
+    ],
+  },
+  {
+    ringId: 3,
+    projects: [
+      { id: 12, name: 'EXAM RANK 3', validated: false },
+      { id: 13, name: 'Philosophers', validated: false },
+      { id: 14, name: 'minishell', validated: false },
+    ],
+  },
+  {
+    ringId: 4,
+    projects: [
+      { id: 15, name: 'EXAM RANK 4', validated: false },
+      { id: 16, name: 'NetPractice', validated: false },
+      { id: 17, name: 'CPP Module 00', validated: false },
+      { id: 18, name: 'CPP Module 01', validated: false },
+      { id: 19, name: 'CPP Module 02', validated: false },
+      { id: 20, name: 'CPP Module 03', validated: false },
+      { id: 21, name: 'CPP Module 04', validated: false },
+      { id: 22, name: 'cub3d', validated: false },
+      { id: 23, name: 'mini-RT', validated: false },
+    ],
+  },
+  {
+    ringId: 5,
+    projects: [
+      { id: 24, name: 'EXAM RANK 5', validated: false },
+      { id: 25, name: 'CPP Module 05', validated: false },
+      { id: 26, name: 'CPP Module 06', validated: false },
+      { id: 27, name: 'CPP Module 07', validated: false },
+      { id: 28, name: 'CPP Module 08', validated: false },
+      { id: 29, name: 'CPP Module 09', validated: false },
+      { id: 30, name: 'Inception', validated: false },
+      { id: 31, name: 'ft_irc', validated: false },
+      { id: 32, name: 'webserv', validated: false },
+    ],
+  },
+  {
+    ringId: 6,
+    projects: [
+      { id: 33, name: 'EXAM RANK 6', validated: false },
+      { id: 34, name: 'ft_transcendence', validated: false },
+    ],
+  },
+]
--- types/42api.d.ts
+++ types/42api.d.ts
@@ -11,9 +11,29 @@ export interface Campus {
 }
 
 export interface ProjectUser {
-  project: {
-    name: string
-  }
+  id: number
+  occurrence: number
+  final_mark: number | null
+  status: 'in_progress' | 'completed' | 'failed' | 'waiting' | 'pending' // Add other possible statuses if needed
+  validated?: boolean | null // Nullable boolean (true = validated, false = not validated, null = not finished)
+  current_team_id: number
+  project: Project
+  cursus_ids: number[]
+  marked_at: string | null
+  marked: boolean
+  retriable_at: string | null
+  created_at: string
+  updated_at: string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any
+}
+export interface Project {
+  id: number
+  name: string
+  slug: string
+  parent_id: number | null
+}
+
+export interface ProjectsUserList {
+  projects_users: ProjectsUser[]
 }
